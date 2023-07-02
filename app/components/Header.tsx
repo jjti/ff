99c724756448c0896733e5390cd8d3fd11eb6f1b
josh <josh.timmons@hashicorp.com>
2023-07-02 19:37:27 -0400

    Add coffee button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Button } from 'antd';
+import { CoffeeOutlined } from '@ant-design/icons';
 
 export default class Header extends React.Component {
   public render() {
@@ -7,9 +9,11 @@ export default class Header extends React.Component {
         <div id="Header-Title">
           <h1 id="App-Header">ffdraft.app</h1>
         </div>
-        <a href="https://github.com/jjtimmons/ff" target="_blank">
-          A value-based fantasy football draft wizard
-        </a>
+        <div>
+          <Button href="https://www.buymeacoffee.com/ffdraft" target="_blank">
+            Support the site <CoffeeOutlined />
+          </Button>
+        </div>
       </div>
     );
   }
